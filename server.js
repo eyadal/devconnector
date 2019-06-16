06c281c3f979dbdfbef074fa092a04fbcf235fe5
Eyad <eyad.al@hotmail.se>
2019-06-16 20:42:44 +0200

    scripts change (start, server)

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
