0ead56a1d797c9c84874a9c57f8919e079666c2b
Eyad <eyad.al@hotmail.se>
2019-06-16 18:33:51 +0200

    added a serverjs file

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
