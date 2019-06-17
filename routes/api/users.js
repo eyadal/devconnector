b70a35c847b41ac3e298871d8bd327ae1f29624b
Eyad <eyad.al@hotmail.se>
2019-06-17 19:12:26 +0200

    bring in bcrypt for password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
