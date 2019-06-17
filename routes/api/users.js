c42de3a7ebcbb337a849c436d770ceb0eb00fd5f
Eyad <eyad.al@hotmail.se>
2019-06-17 21:22:01 +0200

    bring in jsonwebtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
