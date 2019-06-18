2f196c53e8016085e562d88e6c4d6cf6d861c083
Eyad <eyad.al@hotmail.se>
2019-06-18 23:48:55 +0200

    verify token catch error token

--- middleware/auth.js
+++ middleware/auth.js
@@ -18,5 +18,7 @@ module.exports = function(req, res, next) {
     const decoded = jwt.verify(token, config.get('jwtSecret'));
     req.user = decoded.user;
     next();
-  } catch (err) {}
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
 };
