a09545112e6afb38329217dc6df1d567f8f6d2b6
Eyad <eyad.al@hotmail.se>
2019-06-19 00:48:28 +0200

    catch error User.fintById

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -11,7 +11,10 @@ router.get('/', auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
     res.json(user);
-  } catch (err) {}
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
