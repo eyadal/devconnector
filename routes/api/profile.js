8e97e2ba4911075aaeead705162ca5d9ba2f4be1
Eyad <eyad.al@hotmail.se>
2019-06-22 18:31:22 +0200

    bring in profile and user models

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,9 +2,18 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
 // @route       GET api/profile/me
 // description  Get current users profile
 // access       Private
-router.get('/', auth, (req, res) => res.send('Profile route'));
+router.get('/', auth, async (req, res) => {
+  try {
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
