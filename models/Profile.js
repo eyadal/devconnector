99b2bc70b7cec5127cccd9e6b7eaae708ace3a44
Eyad <eyad.al@hotmail.se>
2019-06-22 17:15:38 +0200

    Object with social medias

--- models/Profile.js
+++ models/Profile.js
@@ -87,4 +87,21 @@ const ProfileSchema = new mongoose.Schema({
       }
     }
   ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
 });
