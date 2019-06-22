fca2668b740eb611f397c9ab40b40f5da83de5e4
Eyad <eyad.al@hotmail.se>
2019-06-22 18:21:41 +0200

    bring in auth middleware

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route       GET api/profile
-// description  test route
-// access       public
-router.get('/', (req, res) => res.send('Profile route'));
+// @route       GET api/profile/me
+// description  Get current users profile
+// access       Private
+router.get('/', auth, (req, res) => res.send('Profile route'));
 
 module.exports = router;
