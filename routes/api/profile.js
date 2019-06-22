717e277141a0725f8e6a897ab23144438557ca93
Eyad <eyad.al@hotmail.se>
2019-06-22 23:15:04 +0200

    profileFields object

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -67,6 +67,12 @@ router.post(
     // Build profile obejct
     const profileFields = {};
     profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
   }
 );
 
