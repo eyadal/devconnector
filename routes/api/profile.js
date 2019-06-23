fbe081e9818556cd06f0b328c29d8f923eb6af34
Eyad <eyad.al@hotmail.se>
2019-06-24 00:54:12 +0200

    if statement for validationResult

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -185,6 +185,9 @@ router.put(
   ],
   async (req, res) => {
     const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
   }
 );
 
