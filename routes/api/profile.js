ab5a488562a0a9939d20cd4246aa966a0dd2b671
Eyad <eyad.al@hotmail.se>
2019-06-23 14:41:37 +0200

    create a variable with properties for the route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -114,6 +114,14 @@ router.post(
 // @route       GET api/profile
 // description  Get all profiles
 // access       Public
-router.get('/', async (req, res) => {});
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
