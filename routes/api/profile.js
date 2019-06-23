a8e43987882c7f2e4a0f8394a6dd4bd46f0b7f9b
Eyad <eyad.al@hotmail.se>
2019-06-24 01:57:36 +0200

    send back json response  for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -237,6 +237,7 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
 
     profile.experience.splice(removeIndex, 1);
     await profile.save();
+    res.json(profile);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
