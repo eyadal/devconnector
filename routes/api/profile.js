7133974eeb317f7c084c40ae7ccbd5c0abaf2c96
Eyad <eyad.al@hotmail.se>
2019-06-24 01:46:37 +0200

    console error log

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -226,6 +226,12 @@ router.put(
 // @route       DELETE api/profile/experience/:exp_id
 // description  Delete experience from profile
 // access       Private
-router.delete('/experience/:exp_id', auth, async (req, res) => {});
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
