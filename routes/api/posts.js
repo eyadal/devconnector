3d6e78b7e6c1be8b23e021309f849a433c0487d9
Eyad <eyad.al@hotmail.se>
2019-06-26 00:51:19 +0200

    async, error validationResult with if statement

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -16,7 +16,12 @@ router.post(
         .isEmpty()
     ]
   ],
-  (req, res) => {}
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
 );
 
 module.exports = router;
