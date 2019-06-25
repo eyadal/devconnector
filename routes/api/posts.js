267b054b2e1c3f23225096802441473b14ebfdae
Eyad <eyad.al@hotmail.se>
2019-06-26 00:42:07 +0200

    bring in the auth middleware

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
+const auth = require('../../middleware/auth');
 
 // @route       POST api/posts
 // description  Create a post
