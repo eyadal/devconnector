9fc312130eaf43ae853de9bb1116c89929315535
Eyad <eyad.al@hotmail.se>
2019-06-26 13:53:41 +0200

    catch error

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -204,6 +204,14 @@ router.post(
 // @route       DELETE api/posts/comment/:id/comment_id
 // description  Delete a comment
 // access       Private
-router.delete('comment/:id/comment_id', auth, async (req, res) => {});
+router.delete('comment/:id/comment_id', auth, async (req, res) => {
+  const post = await Post.findById(req.params.id);
+
+  try {
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
