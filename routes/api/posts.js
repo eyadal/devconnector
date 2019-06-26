5855ccb7d12b15e08932f539ed1b7043e240ee4d
Eyad <eyad.al@hotmail.se>
2019-06-26 03:42:38 +0200

    variable for unlike a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -130,4 +130,33 @@ router.put('/like/:id', auth, async (req, res) => {
   }
 });
 
+// @route       PUT api/posts//unlike/:id
+// description  Like a post
+// access       Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if the post has already been liked
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res.status(400).json({ msg: 'Post has not yet been liked' });
+    }
+
+    // Get remove index
+    const removeIndex = post.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Status Error');
+  }
+});
+
 module.exports = router;
