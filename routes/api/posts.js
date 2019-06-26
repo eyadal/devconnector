a27ebeefea2c437ba4c147f2c3c00c30bac2b44b
Eyad <eyad.al@hotmail.se>
2019-06-26 03:27:37 +0200

    adds the post like if it has not beed liked

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -119,6 +119,8 @@ router.put('/like/:id', auth, async (req, res) => {
     ) {
       return res.status(400).json({ msg: 'Post already liked' });
     }
+
+    post.likes.unshift({ user: req.user.id });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Status Error');
