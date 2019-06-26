f035a43169091ec534d15b6d3560e82fe4bdb765
Eyad <eyad.al@hotmail.se>
2019-06-26 02:52:59 +0200

    try if statement post not found

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -86,6 +86,10 @@ router.delete('/:id', auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
 
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
     // Check user
     if (post.user.toString() !== req.user.id) {
       return res.status(401).json({ msg: 'User not authorized' });
