baa5a81db7fa61b29572b7d6173220ace265776b
Eyad <eyad.al@hotmail.se>
2019-06-26 03:43:40 +0200

    remove post using splice

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -150,6 +150,8 @@ router.put('/unlike/:id', auth, async (req, res) => {
       .map(like => like.user.toString())
       .indexOf(req.user.id);
 
+    post.likes.splice(removeIndex, 1);
+
     await post.save();
 
     res.json(post.likes);
