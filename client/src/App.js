3ee9f605b5ad8635c24e442d1d91d1917a597e77
Eyad <eyad.al@hotmail.se>
2019-06-27 02:12:27 +0200

    created Route for Register & Login

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,12 @@ const App = () => (
     <Fragment>
       <Navbar />
       <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
     </Fragment>
   </Router>
 );
