5dba5333833600f5a3cbacd7bb0c8c7f7c6346d7
Eyad <eyad.al@hotmail.se>
2019-06-27 03:54:20 +0200

    uppdated

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -18,24 +18,7 @@ const Register = () => {
     if (password !== password2) {
       console.log('Passwords do not match');
     } else {
-      const newUser = {
-        name,
-        email,
-        password
-      };
-
-      try {
-        const config = {
-          headers: {
-            'Content-Type': ' application/json'
-          }
-        };
-        const body = JSON.stringify(newUser);
-        const res = await axios.post('/api/users', body, config);
-        console.log(res.data);
-      } catch (err) {
-        console.error(err.response.data);
-      }
+      console.log('SUCCES');
     }
   };
 
