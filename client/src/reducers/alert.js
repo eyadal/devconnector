d19f2ce2c073caf2f5314402acd681f5c3bfab78
Eyad <eyad.al@hotmail.se>
2019-06-27 23:25:15 +0200

    alert.js with some objects

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -3,8 +3,14 @@ import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 const initialState = {};
 
 export default function(state = initialState, action) {
-  switch (action.type) {
+  const { type, payload } = action;
+
+  switch (type) {
     case SET_ALERT:
-      return [...state, action.payload];
+      return [...state, payload];
+    case REMOVE_ALERT:
+      return state.filter(alert => alert.id !== payload);
+    default:
+      return state;
   }
 }
