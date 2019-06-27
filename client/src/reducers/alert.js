658483d3fd7efb74ed90f972f9cc5022b7735adb
Eyad <eyad.al@hotmail.se>
2019-06-27 23:12:52 +0200

    import set/remove alert, and export a function

---
+++ client/src/reducers/alert.js
@@ -0,0 +1,9 @@
+import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
+
+const initialState = {};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case SET_ALERT:
+  }
+}
