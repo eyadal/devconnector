0c896576bd477d9be4e433e115afdaf20f42fcfb
Eyad <eyad.al@hotmail.se>
2019-06-27 03:37:14 +0200

    newUser obejct

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -13,12 +13,16 @@ const Register = () => {
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
       console.log('Passwords do not match');
     } else {
-      console.log(formData);
+      const newUser = {
+        name,
+        email,
+        password
+      };
     }
   };
 
