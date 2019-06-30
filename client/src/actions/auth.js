c6953c339c88077d331bba8564863ec9f95afbd1
Eyad <eyad.al@hotmail.se>
2019-06-30 02:32:20 +0200

    register the user component

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,3 +2,4 @@ import axios from 'axios';
 import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
 
 //Register User
+export const register = ({ name, email, password }) => async dispatch => {};
