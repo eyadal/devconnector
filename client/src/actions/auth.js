8539280c1c6da788c750998a64647c6f48815803
Eyad <eyad.al@hotmail.se>
2019-06-30 02:36:29 +0200

    await axios.post from api/users

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -10,4 +10,8 @@ export const register = ({ name, email, password }) => async dispatch => {
   };
 
   const body = JSON.stringify({ name, email, password });
+
+  try {
+    const res = await axios.post('/api/users', body, config);
+  } catch (err) {}
 };
