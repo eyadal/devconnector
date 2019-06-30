e21c49e8cfc339e3167af642bb7a30605920cec1
Eyad <eyad.al@hotmail.se>
2019-07-01 01:35:35 +0200

    else delete token

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -3,5 +3,7 @@ import axios from 'axios';
 const setAuthToken = token => {
   if (token) {
     axios.defaults.headers.common['x-auth-token'] = token;
+  } else {
+    delete axios.defaults.headers.common['x-atuh-token'];
   }
 };
