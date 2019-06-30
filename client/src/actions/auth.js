e8a26da6e782d599eb5e590ab44dcabc2d4587c7
Eyad <eyad.al@hotmail.se>
2019-06-30 02:34:23 +0200

    const for the config and the body

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,4 +2,12 @@ import axios from 'axios';
 import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
 
 //Register User
-export const register = ({ name, email, password }) => async dispatch => {};
+export const register = ({ name, email, password }) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  const body = JSON.stringify({ name, email, password });
+};
