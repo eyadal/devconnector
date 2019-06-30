df35ddcd9e61cb995ce451726da9b4f0a29a53d8
Eyad <eyad.al@hotmail.se>
2019-07-01 01:46:26 +0200

    send the payload to USER_LOADED

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -16,6 +16,11 @@ export const loadUser = () => async dispatch => {
 
   try {
     const res = await axios.get('/api/auth');
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data
+    });
   } catch (err) {}
 };
 
