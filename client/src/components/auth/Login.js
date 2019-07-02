28a4ca7bcff28161f8ff0073c5cca1cf043d9dee
Eyad <eyad.al@hotmail.se>
2019-07-03 01:15:03 +0200

    pull login in out of props

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import login from '../../actions/auth';
 
-const Login = () => {
+const Login = ({ login }) => {
   const [formData, setFormData] = useState({
     email: '',
     password: ''
