d3239a0e1ff67b23f720f45a2652aee880b319cb
Eyad <eyad.al@hotmail.se>
2019-07-03 01:01:31 +0200

    login user

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -56,3 +56,32 @@ export const register = ({ name, email, password }) => async dispatch => {
     });
   }
 };
+
+//Login User
+export const login = (email, password) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post('/api/auth', body, config);
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: LOGIN_FAIL
+    });
+  }
+};
