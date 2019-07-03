5710544479647d7ddf914cf11b8247f2a2c2a6fb
Eyad <eyad.al@hotmail.se>
2019-07-03 14:16:55 +0200

    import couple of lines, and export default.

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
 const Navbar = () => {
   return (
@@ -24,4 +27,7 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default connect(
+  mapStateToProps,
+  { logout }
+)(Navbar);
