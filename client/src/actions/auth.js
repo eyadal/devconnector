d359fdaf4ad1efc7fc27cfcdc640e39413cdb09c
Eyad <eyad.al@hotmail.se>
2019-07-03 14:10:28 +0200

    export logout and dispatch it with LOGOUT

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -90,4 +91,11 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_FAIL
     });
   }
+};
+
+// Export / Clear Profile
+export const logout = () => dispath => {
+  dispatch({
+    type: LOGOUT
+  });
 };
