63251a0e529c42c6aa1bc5c1d0e2adf6fd311ab3
Eyad <eyad.al@hotmail.se>
2019-07-03 23:29:06 +0200

    imports, and props

---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const PrivateRoute = ({ component: Component, auth, ...rest }) => <Route />;
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
