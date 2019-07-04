7ca1fb45e61df406f815a2b1815ff17602fbc50e
Eyad <eyad.al@hotmail.se>
2019-07-04 20:08:30 +0200

    export getCurrentProfile with async and dispatch

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,3 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
 import { GET_PROFILE, PROFILE_ERROR } from './types';
+
+// Get current users profile
+export const getCurrentProfile = () => async dispatch => {};
