4b77f954cf545a4e90be8ca0ead2a579f80f6b66
Eyad <eyad.al@hotmail.se>
2019-07-04 20:42:10 +0200

    create switch for GET_PROFILE & PROFILE_ERROR and return state and payload

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,3 +1,5 @@
+import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+
 const initialState = {
   profile: null,
   profiles: [],
@@ -10,5 +12,19 @@ export default function(state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false
+      };
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false
+      };
+    default:
+      return state;
   }
 }
