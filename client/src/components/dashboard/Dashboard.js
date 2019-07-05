8f1d0678169b65b149a85c080adcd307faf99ffc
Eyad <eyad.al@hotmail.se>
2019-07-05 19:17:39 +0200

    import spinner to dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
 
 const Dashboard = ({ getCurrentProfile, auth, profile }) => {
