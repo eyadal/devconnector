901eb3bed7c911df1cdf5328d5ff25ca95b391f0
Eyad <eyad.al@hotmail.se>
2019-07-05 18:38:10 +0200

    import connect & proptypes

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 const Landing = () => {
   return (
