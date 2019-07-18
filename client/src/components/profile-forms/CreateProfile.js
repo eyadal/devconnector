c76cb59a2ff807a10f87b0285c7c5f051fc7f8b3
Eyad <eyad.al@hotmail.se>
2019-07-18 23:58:58 +0200

    const with array formData and setFormData with useState hook

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -3,6 +3,21 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 const CreateProfile = props => {
+  const [formData, setFormData] = useState({
+    company: '',
+    website: '',
+    location: '',
+    status: '',
+    skills: '',
+    githubusername: '',
+    bio: '',
+    twitter: '',
+    facebook: '',
+    linkedin: '',
+    youtube: '',
+    instagram: ''
+  });
+
   return <div />;
 };
 
