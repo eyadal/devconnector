4ea9a7b6c1e5404340b06d31e7d8dec72b0609c7
Eyad <eyad.al@hotmail.se>
2019-07-18 23:52:19 +0200

    import lines  for creating forms for create profile

---
+++ client/src/components/profile-forms/CreateProfile.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const CreateProfile = props => {
+  return <div />;
+};
+
+CreateProfile.propTypes = {};
+
+export default CreateProfile;
