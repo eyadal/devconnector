e8ac6f7071c4f5ffafbfccdb62cca42a9341cbb0
Eyad <eyad.al@hotmail.se>
2019-07-23 03:46:54 +0200

    racfp and props

---
+++ client/src/components/dashboard/Experience.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Experience = ({ experience }) => {
+  return <div />;
+};
+
+Experience.propTypes = {};
+
+export default Experience;
