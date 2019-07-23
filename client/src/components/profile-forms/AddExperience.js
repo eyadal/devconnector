9ff6438417dd4902a9308207c1156dd2dcb4f96f
Eyad <eyad.al@hotmail.se>
2019-07-23 02:40:08 +0200

    import & export

---
+++ client/src/components/profile-forms/AddExperience.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addExperience } from '../../actions/profile';
+
+const AddExperience = props => {
+  return <div />;
+};
+
+AddExperience.propTypes = {};
+
+export default connect(
+  null,
+  { addExperience }
+)(AddExperience);
