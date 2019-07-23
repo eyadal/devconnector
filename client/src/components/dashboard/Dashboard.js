f4a6839594b0c2ee2b8443c41740cd96e109c73a
Eyad <eyad.al@hotmail.se>
2019-07-24 00:53:46 +0200

    add deleteAccount to props

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -10,6 +10,7 @@ import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 
 const Dashboard = ({
   getCurrentProfile,
+  deleteAccount,
   auth: { user },
   profile: { profile, loading }
 }) => {
@@ -33,7 +34,8 @@ const Dashboard = ({
           <Education education={profile.education} />
           <div className='my-2'>
             <button className='btn btn-danger' onClick={() => deleteAccount()}>
-              <i className='fas fa-user-minus' />Delete My Account
+              <i className='fas fa-user-minus' />
+              Delete My Account
             </button>
           </div>
         </Fragment>
