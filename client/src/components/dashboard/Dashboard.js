e477226cfb0c4b8f2e1c932c1f29391216577a6f
Eyad <eyad.al@hotmail.se>
2019-07-23 04:09:11 +0200

    import Experience

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
 import { getCurrentProfile } from '../../actions/profile';
 
 const Dashboard = ({
