e79a89ced18a01685417ccb2ff57a77935bf64ba
Eyad <eyad.al@hotmail.se>
2019-07-25 16:07:33 +0200

    corrected

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,11 +3,11 @@ import { setAlert } from './alert';
 
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_ERROR,
   UPDATE_PROFILE,
   CLEAR_PROFILE,
-  ACCOUNT_DELETED,
-  GET_PROFILES
+  ACCOUNT_DELETED
 } from './types';
 
 // Get current users profile
