da528c03f068a5ee53af954ecc258baedd9da771
Eyad <eyad.al@hotmail.se>
2019-07-25 17:00:45 +0200

    corrected

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { connecct } from 'react-redux';
+import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfiles } from '../../actions/profile';
 
