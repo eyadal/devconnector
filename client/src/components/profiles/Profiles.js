4b5b79247230559a35285c00bdea62eed7e5a442
Eyad <eyad.al@hotmail.se>
2019-07-25 16:46:03 +0200

    import a couple of things

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,13 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connecct } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getProfiles } from '../../actions/profile';
+
+const Profiles = props => {
+  return <div />;
+};
+
+Profiles.propTypes = {};
+
+export default Profiles;
