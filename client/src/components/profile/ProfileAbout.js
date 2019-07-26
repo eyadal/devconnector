891ec26b8d45a17a2191c66f28dd6f3bd0fbfd58
Eyad <eyad.al@hotmail.se>
2019-07-26 19:01:56 +0200

    bio only if the profile has a bio

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -7,16 +7,16 @@ const ProfileAbout = ({
     skills,
     user: { name }
   }
-}) => {
+}) => (
   <div class='profile-about bg-light p-2'>
-    {bio && <Fragment />}
-    <h2 class='text-primary'>{name}</h2>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed doloremque
-      nesciunt, repellendus nostrum deleniti recusandae nobis neque modi
-      perspiciatis similique?
-    </p>
-    <div class='line' />
+    {bio && (
+      <Fragment>
+        <h2 class='text-primary'>{name}s Bio</h2>
+        <p>{bio}</p>
+        <div class='line' />
+      </Fragment>
+    )}
+
     <h2 class='text-primary'>Skill Set</h2>
     <div class='skills'>
       <div class='p-1'>
@@ -35,8 +35,8 @@ const ProfileAbout = ({
         <i class='fa fa-check' /> C#
       </div>
     </div>
-  </div>;
-};
+  </div>
+);
 
 ProfileAbout.propTypes = {
   profile: PropTypes.object.isRequired
