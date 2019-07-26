43bf3432d77fb01aa34641a27b687c36bb072b79
Eyad <eyad.al@hotmail.se>
2019-07-26 18:44:43 +0200

    corrected

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -25,22 +25,33 @@ const ProfileTop = ({
             <i class='fas fa-globe fa-2x' />
           </a>
         )}
+        {social && social.twitter && (
+          <a href={social.twitter} target='_blank' rel='noopener noreferrer'>
+            <i class='fab fa-twitter fa-2x' />
+          </a>
+        )}
+        {social && social.facebook && (
+          <a href='#' target='_blank' rel='noopener noreferrer'>
+            <i class='fab fa-facebook fa-2x' />
+          </a>
+        )}
+        {social && social.linkedin && (
+          <a href={social.linkedin} target='_blank' rel='noopener noreferrer'>
+            <i class='fab fa-linkedin fa-2x' />
+          </a>
+        )}
 
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i class='fab fa-twitter fa-2x' />
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i class='fab fa-facebook fa-2x' />
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i class='fab fa-linkedin fa-2x' />
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i class='fab fa-youtube fa-2x' />
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i class='fab fa-instagram fa-2x' />
-        </a>
+        {social && social.youtube && (
+          <a href={social.youtube} target='_blank' rel='noopener noreferrer'>
+            <i class='fab fa-youtube fa-2x' />
+          </a>
+        )}
+
+        {social && social.instagram && (
+          <a href={social.instagram} target='_blank' rel='noopener noreferrer'>
+            <i class='fab fa-instagram fa-2x' />
+          </a>
+        )}
       </div>
     </div>
   );
