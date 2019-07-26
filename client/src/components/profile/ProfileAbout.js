f69d54aeafd5f325b2ef16f866941c8bbc8aafaa
Eyad <eyad.al@hotmail.se>
2019-07-26 18:49:42 +0200

    destruct from profile props, for PRofileAbout

---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ProfileAbout = ({
+  profile: {
+    bio,
+    skills,
+    user: { name }
+  }
+}) => {
+  return <div />;
+};
+
+ProfileAbout.propTypes = {};
+
+export default ProfileAbout;
