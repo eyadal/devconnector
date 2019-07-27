a93d31cfe4f8c207e5142d89d37d05b4e64d134a
Eyad <eyad.al@hotmail.se>
2019-07-27 03:15:51 +0200

    import

---
+++ client/src/components/profile/ProfileGithub.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getGithubRepos } from '../../actions/profile';
+
+const ProfileGithub = props => {
+  return <div />;
+};
+
+ProfileGithub.propTypes = {};
+
+export default ProfileGithub;
