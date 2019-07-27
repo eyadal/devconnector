8c6ef833083590c021574dd1b84fbc6a31219214
Eyad <eyad.al@hotmail.se>
2019-07-27 02:34:59 +0200

    new file created, import moment

---
+++ client/src/components/profile/ProfileExperience.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+
+const ProfileExperience = props => {
+  return <div />;
+};
+
+ProfileExperience.propTypes = {};
+
+export default ProfileExperience;
