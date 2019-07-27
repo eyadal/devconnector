9d7659fee7b7f64e68c44e6b53812727e24d7c47
Eyad <eyad.al@hotmail.se>
2019-07-27 03:32:14 +0200

    some divs and other tags to map the github repos

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -5,7 +5,33 @@ import Spinner from '../layout/Spinner';
 import { getGithubRepos } from '../../actions/profile';
 
 const ProfileGithub = ({ username, getGithubRepos, repos }) => {
-  return <div />;
+  useEffect(() => {
+    getGithubRepos(username);
+  }, [getGithubRepos]);
+  return (
+    <div className='profile-github'>
+      <h2 className='text-primary my-1'>Github Repos</h2>
+      {repos === null ? (
+        <Spinner />
+      ) : (
+        repos.map(repo => (
+          <div key={repo._id} className='repo bg-white p-1 my-1'>
+            <div>
+              <h4>
+                <a
+                  href={repo.html_url}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  {repo.name}
+                </a>
+              </h4>
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 ProfileGithub.propTypes = {
