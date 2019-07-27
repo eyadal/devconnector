55e649c9bd0cbe2887d389ed8cd83ae677bacbaf
Eyad <eyad.al@hotmail.se>
2019-07-27 02:36:52 +0200

    destruct from experience

--- client/src/components/profile/ProfileExperience.js
+++ client/src/components/profile/ProfileExperience.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
 
-const ProfileExperience = props => {
+const ProfileExperience = ({
+  experience: { company, title, location, current, to, from, description }
+}) => {
   return <div />;
 };
 
