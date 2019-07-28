64e021361260994d774bc16233da21cfc8f410b2
Eyad <eyad.al@hotmail.se>
2019-07-28 16:42:38 +0200

    rcafp with couple of import

---
+++ client/src/components/posts/Post.js
@@ -0,0 +1,13 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getPosts } from '../../actions/post';
+
+const Post = props => {
+  return <div />;
+};
+
+Post.propTypes = {};
+
+export default Post;
