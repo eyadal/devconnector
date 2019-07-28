313b185f04ef083d6d1add04aa655d404ad8ab52
Eyad <eyad.al@hotmail.se>
2019-07-28 23:31:57 +0200

    onSubmit, and addPost{text}

--- client/src/components/posts/PostForm.js
+++ client/src/components/posts/PostForm.js
@@ -10,7 +10,13 @@ const PostForm = ({ addPost }) => {
       <div class='bg-primary p'>
         <h3>Say Something...</h3>
       </div>
-      <form class='form my-1'>
+      <form
+        class='form my-1'
+        onSubmit={e => {
+          e.preventDefault();
+          addPost({ text });
+        }}
+      >
         <textarea
           name='text'
           cols='30'
