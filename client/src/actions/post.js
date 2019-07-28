9cab09682821ad00ff84582fd92a11652009e227
Eyad <eyad.al@hotmail.se>
2019-07-29 00:12:32 +0200

    getPost created in the post.js actions

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -101,3 +101,20 @@ export const addPost = formData => async dispatch => {
     });
   }
 };
+
+// Get Post
+export const getPost = () => async dispatch => {
+  try {
+    const res = await axios.get(`/api/posts/${id}`);
+
+    dispatch({
+      type: GET_POST,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
