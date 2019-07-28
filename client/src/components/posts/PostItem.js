5b78d1c41064286bebab42359098371688710a39
Eyad <eyad.al@hotmail.se>
2019-07-28 20:28:36 +0200

    postItem form

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,14 +3,44 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
-const PostItem = props => {
-  return <div />;
-};
+const PostItem = ({
+  auth,
+  post: { _id, text, name, avatar, user, likes, comments, date }
+}) => (
+  <div class='post bg-white p-1 my-1'>
+    <div>
+      <Link to='profile.html'>
+        <img class='round-img' src={avatar} alt='' />
+        <h4>{name}</h4>
+      </Link>
+    </div>
+    <div>
+      <p class='my-1'>{text}</p>
+      <p class='post-date'>
+        Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
+      </p>
+      <button type='button' class='btn btn-light'>
+        <i class='fas fa-thumbs-up' />
+        <span>{likes.length}</span>
+      </button>
+      <button type='button' class='btn btn-light'>
+        <i class='fas fa-thumbs-down' />
+      </button>
+      <Link to='post.html' class='btn btn-primary'>
+        Discussion <span class='comment-count'>{comments.length}</span>
+      </Link>
+      {!auth.loading && user === auth.user._id && (
+        <button type='button' class='btn btn-danger'>
+          <i class='fas fa-times' />
+        </button>
+      )}
+    </div>
+  </div>
+);
 
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
-  auth: PropTypes.object.isRequired,
-  e
+  auth: PropTypes.object.isRequired
 };
 const mapStateToProps = state => ({
   auth: state.auth
