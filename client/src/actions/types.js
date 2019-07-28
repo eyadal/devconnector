4343d7ce5034e0c5b80f337725af3d0cdbfaf315
Eyad <eyad.al@hotmail.se>
2019-07-28 23:02:46 +0200

    export ADD_POST

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -18,3 +18,4 @@ export const PROFILE_ERROR = 'PROFILE_ERROR';
 export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
 export const UPDATE_LIKES = 'UPDATE_LIKES';
 export const DELETE_POST = 'DELETE_POST';
+export const ADD_POST = 'ADD_POST';
