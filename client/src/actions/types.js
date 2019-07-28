b02a0964c2777f66ec12e824b7d464afef6fccce
Eyad <eyad.al@hotmail.se>
2019-07-28 22:46:05 +0200

    export DELETE_POST

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -17,3 +17,4 @@ export const CLEAR_PROFILE = 'CLEAR_PROFILE';
 export const PROFILE_ERROR = 'PROFILE_ERROR';
 export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
 export const UPDATE_LIKES = 'UPDATE_LIKES';
+export const DELETE_POST = 'DELETE_POST';
