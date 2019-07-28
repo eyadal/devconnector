70dce1c5f5aae9e5d52b86e484512a23c5cfe74f
Eyad <eyad.al@hotmail.se>
2019-07-29 00:28:21 +0200

    import some stuff

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,13 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../../components/layout/Spinner';
+import { getPost } from '../../actions/post';
+
+const Post = props => {
+  return <div />;
+};
+
+Post.propTypes = {};
+
+export default Post;
