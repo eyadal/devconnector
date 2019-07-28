6ca00f44bbea529f9646997a681d007b979bb517
Eyad <eyad.al@hotmail.se>
2019-07-28 18:42:39 +0200

    corrected

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -9,21 +9,21 @@ const initialState = {
 
 export default function(state = initialState, action) {
   const { type, payload } = action;
-}
 
-switch (type) {
-  case GET_POSTS:
-    return {
-      ...state,
-      posts: payload,
-      loading: false
-    };
-  case POST_ERROR:
-    return {
-      ...state,
-      error: payload,
-      loading: false
-    };
-  default:
-    return state;
+  switch (type) {
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: payload,
+        loading: false
+      };
+    case POST_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false
+      };
+    default:
+      return state;
+  }
 }
