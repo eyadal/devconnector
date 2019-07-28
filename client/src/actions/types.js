d977f31e0ad118cea36777fa9449d064527f2d24
Eyad <eyad.al@hotmail.se>
2019-07-29 00:10:43 +0200

    export GET_POST

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -11,6 +11,7 @@ export const GET_PROFILE = 'GET_PROFILE';
 export const GET_PROFILES = 'GET_PROFILES';
 export const GET_REPOS = 'GET_REPOS';
 export const GET_POSTS = 'GET_POSTS';
+export const GET_POST = 'GET_POST';
 export const POST_ERROR = 'POST_ERROR';
 export const UPDATE_PROFILE = 'UPDATE_PROFILE';
 export const CLEAR_PROFILE = 'CLEAR_PROFILE';
