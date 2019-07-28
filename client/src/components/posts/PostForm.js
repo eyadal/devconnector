f27da8423af1188e7bf2dc7ea74413865c79cdcc
Eyad <eyad.al@hotmail.se>
2019-07-28 23:30:28 +0200

    value{text} added, and a onChange for the setText

--- client/src/components/posts/PostForm.js
+++ client/src/components/posts/PostForm.js
@@ -5,7 +5,25 @@ import { addPost } from '../../actions/post';
 
 const PostForm = ({ addPost }) => {
   const [text, setText] = useState('');
-  return <div />;
+  return (
+    <div class='post-form'>
+      <div class='bg-primary p'>
+        <h3>Say Something...</h3>
+      </div>
+      <form class='form my-1'>
+        <textarea
+          name='text'
+          cols='30'
+          rows='5'
+          placeholder='Create a post'
+          value={text}
+          onChange={e => setText(e.target.value)}
+          required
+        />
+        <input type='submit' class='btn btn-dark my-1' value='Submit' />
+      </form>
+    </div>
+  );
 };
 
 PostForm.propTypes = {
