4748522a5de0dafd693322d96abca59df24b2e61
Eyad <eyad.al@hotmail.se>
2019-07-28 21:50:53 +0200

    case for UPDATE_LIKES and manipulate posts and map threw it

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -23,6 +23,19 @@ export default function(state = initialState, action) {
         error: payload,
         loading: false
       };
+    case UPDATE_LIKES:
+      return {
+        ...state,
+        posts: state.posts.map(post =>
+          post._id === payload.id
+            ? {
+                ...post,
+                likes: payload.likes
+              }
+            : post
+        ),
+        loading: false
+      };
     default:
       return state;
   }
