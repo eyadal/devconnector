8723233566828cf50015a2e21c3dc783ecb831c3
Eyad <eyad.al@hotmail.se>
2019-07-28 16:30:01 +0200

    export getPosts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,3 +1,9 @@
 import axios from 'axios';
 import { setAlert } from './alert';
 import { GET_POSTS, POST_ERROR } from './types';
+
+// Get Posts
+export const getPosts = () => async dispatch => {
+  try {
+  } catch (err) {}
+};
