cff9cac4c772fc50b3896f37015b790075d1348a
Eyad <eyad.al@hotmail.se>
2019-07-29 01:53:46 +0200

    corrected

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,15 +1,14 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Spinner from '../../components/layout/Spinner';
+import Spinner from '../layout/Spinner';
 import { getPost } from '../../actions/post';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
-  useEffekt(() => {
+  useEffect(() => {
     getPost(match.params.id);
   }, [getPost]);
-  return;
-  <div>post</div>;
+  return <div>post</div>;
 };
 
 Post.propTypes = {
