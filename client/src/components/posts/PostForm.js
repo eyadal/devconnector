9776604ec4a7d03a27d4254a262e5a7ed52891a4
Eyad <eyad.al@hotmail.se>
2019-07-28 23:24:48 +0200

    import, connect, propTypes and props for the addPost

---
+++ client/src/components/posts/PostForm.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addPost } from '../../actions/post';
+
+const PostForm = ({ addPost }) => {
+  return <div />;
+};
+
+PostForm.propTypes = {
+  addPost: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { addPost }
+)(PostForm);
