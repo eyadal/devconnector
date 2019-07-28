7e583870632ee8418a1b8412c1077cfbf77fd81c
Eyad <eyad.al@hotmail.se>
2019-07-28 21:58:23 +0200

    id updated

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -20,9 +20,9 @@ export const getPosts = () => async dispatch => {
 };
 
 // Add Like
-export const addLike = postId => async dispatch => {
+export const addLike = id => async dispatch => {
   try {
-    const res = await axios.put(`/api/posts/like/${postId}`);
+    const res = await axios.put(`/api/posts/like/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
@@ -37,9 +37,9 @@ export const addLike = postId => async dispatch => {
 };
 
 // Remove Like
-export const removeLike = postId => async dispatch => {
+export const removeLike = id => async dispatch => {
   try {
-    const res = await axios.put(`/api/posts/unlike/${postId}`);
+    const res = await axios.put(`/api/posts/unlike/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
