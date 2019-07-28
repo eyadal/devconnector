bebd157664416e470c6a648aa6af50fd71045672
Eyad <eyad.al@hotmail.se>
2019-07-28 16:32:16 +0200

    try catch with axios.get and a dispatch for GET_POSTS

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -5,5 +5,11 @@ import { GET_POSTS, POST_ERROR } from './types';
 // Get Posts
 export const getPosts = () => async dispatch => {
   try {
+    const res = await axios.get('/api/posts');
+
+    dispatch({
+      type: GET_POSTS,
+      payload: res.data
+    });
   } catch (err) {}
 };
