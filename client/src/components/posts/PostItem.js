2ddecab9969a5c72225fde4a2698d1b71bd5de34
Eyad <eyad.al@hotmail.se>
2019-07-29 00:09:58 +0200

    changed so the avatar directs to the user and not the profil.html

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -15,7 +15,7 @@ const PostItem = ({
   <div class='posts'>
     <div class='post bg-white p-1 my-1'>
       <div>
-        <Link to='profile.html'>
+        <Link to={`/profile/${user}`}>
           <img class='round-img' src={avatar} alt='' />
           <h4>{name}</h4>
         </Link>
