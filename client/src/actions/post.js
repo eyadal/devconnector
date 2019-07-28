2f96866c8d7037cc72eb41045044e8fd2f251f47
Eyad <eyad.al@hotmail.se>
2019-07-28 21:37:02 +0200

    addLike, and postId for like a specific like

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -18,3 +18,20 @@ export const getPosts = () => async dispatch => {
     });
   }
 };
+
+// Add Like
+export const addLike = postId => async dispatch => {
+  try {
+    const res = await axios.put(`/api/posts/like/${postId}`);
+
+    dispatch({
+      type: GET_POSTS,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
