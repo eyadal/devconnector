35ca4ee496e03364fc9b05e014c14560072b6be9
Eyad <eyad.al@hotmail.se>
2019-07-29 03:19:15 +0200

    updated

--- client/src/components/post/CommentForm.js
+++ client/src/components/post/CommentForm.js
@@ -22,7 +22,7 @@ const CommentForm = ({ postId, addComment }) => {
           name='text'
           cols='30'
           rows='5'
-          placeholder='Create a post'
+          placeholder='Create a comment'
           value={text}
           onChange={e => setText(e.target.value)}
           required
