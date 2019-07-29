dc786139e97f9826f8fdf10bf21706d00d20846a
Eyad <eyad.al@hotmail.se>
2019-07-29 02:55:55 +0200

    export const deleteComment in post.js actions

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -149,3 +149,22 @@ export const addComment = (postId, formData) => async dispatch => {
     });
   }
 };
+
+// Delete Comment
+export const deleteComment = (postId, commentId) => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+    dispatch({
+      type: REMOVE_COMMENT,
+      payload: commentId
+    });
+
+    dispatch(setAlert('Comment Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
