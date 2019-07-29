d567f51f80a776593fa731c79c586497ac26a3d2
Eyad <eyad.al@hotmail.se>
2019-07-29 02:39:42 +0200

    postItem without showing buttons and a go back to posts button

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,14 +1,25 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Spinner from '../layout/Spinner';
+import PostItem from '../posts/PostItem';
 import { getPost } from '../../actions/post';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
     getPost(match.params.id);
   }, [getPost]);
-  return <div>post</div>;
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to='/posts' className='btn'>
+        Back To Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+    </Fragment>
+  );
 };
 
 Post.propTypes = {
